655658e05fce7c52fc5ddcfb63e3eb9b1df5085a
rahul <rahulpatil.m30@gmail.com>
2021-09-10 12:17:16 -0400

    updated static assets

--- src/components/core/AvailableCakes.js
+++ src/components/core/AvailableCakes.js
@@ -1,13 +1,114 @@
 import classes from './AvailableCakes.module.css';
 import BakeItem from './cakeItems/BakeItem';
 import { useEffect, useState } from 'react';
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from '../../App';
-
+import carrotCake from '../../assets/carrot_cake.jpeg'
+import rasmalaiCake from '../../assets/rasmalai_cake.jpeg'
+import blackForestCake from '../../assets/black_forest_cake.jpg'
+import babyShowerCake from '../../assets/baby_shower_cake.jpeg'
+import chocolateCake from '../../assets/chocolate_cake.jpg'
+import chocolateWalnutCake from '../../assets/chocolate_walnut_cake.jpg'
+import gulabJamunCake from '../../assets/gulab_jamun_cake.jpg'
+import lemonCoconutCake from '../../assets/Lemon coconut.jpg'
+import lemonCake from '../../assets/lemon_cake.jpg'
+import mangoCheeseCake from '../../assets/mango_cheese_cake.jpg'
+import pineappleCake from '../../assets/pineapple_cake.jpg'
+import strawberryCake from '../../assets/strawberry_cake.jpg'
 
 const AvailableCakes = () => {
+  const DUMMY_DATA = [
+    {
+      id: 'm1',
+      name: 'Pineapple Cake',
+      description: 'Finest Cake',
+      price: 52.99,
+      img: pineappleCake
+    },
+    {
+      id: 'm2',
+      name: 'Carrot Cake',
+      description: 'A german specialty!',
+      price: 48.5,
+      img: carrotCake
+    },
+    {
+      id: 'm3',
+      name: 'Rasmalai Cake',
+      description: 'Indian, savvy!',
+      price: 62.99,
+      img: rasmalaiCake
+    },
+    {
+      id: 'm4',
+      name: 'Black Forest',
+      description: 'Chocolate King!',
+      price: 48.99,
+      img: blackForestCake
+    },
+    {
+      id: 'm5',
+      name: 'Mango Cake',
+      description: 'Seasonal Summer fever!',
+      price: 58.99,
+      img: mangoCheeseCake
+    },
+    {
+      id: 'm6',
+      name: 'Strawberry Cake',
+      description: 'Chocolate King!',
+      price: 48.99,
+      img: strawberryCake
+    },
+    {
+      id: 'm7',
+      name: 'BabyShower Theme',
+      description: 'Cream Cheese Frosting!',
+      price: 48.99,
+      img: babyShowerCake
+    },
+    {
+      id: 'm8',
+      name: 'Gulab Jamun Cake',
+      description: 'Cream Cheese Frosting!',
+      price: 48.99,
+      img: gulabJamunCake
+    },
+    {
+      id: 'm9',
+      name: 'Lemon Cake',
+      description: 'Cream Cheese Frosting!',
+      price: 48.99,
+      img: lemonCake
+    },
+    {
+      id: 'm10',
+      name: 'Chocolate Cake',
+      description: 'Cream Cheese Frosting!',
+      price: 48.99,
+      img: chocolateCake
+    },
+    {
+      id: 'm11',
+      name: 'Lemon Coconut Cake',
+      description: 'Awesome One!',
+      price: 68.99,
+      img: lemonCoconutCake
+    },
+    {
+      id: 'm12',
+      name: 'Muffins',
+      description: 'Added Bakery!',
+      price: 10.99,
+      img: chocolateWalnutCake
+    },
+  ];
+
+  console.log(DUMMY_DATA);
   const [products, setProducts ] = useState([]);
+
   useEffect(() => {
+    
     const getProducts = async () => {
       const querySnapshot = await getDocs(collection(db, "products"));
       const data = [];
@@ -16,9 +117,7 @@ const AvailableCakes = () => {
         temp.key = doc.id;
         data.push(doc.data());
       });
-      console.log(data);
       setProducts(data);
-      console.log(products);
     }
     getProducts();
 
@@ -32,94 +131,8 @@ const AvailableCakes = () => {
     //   });
     // }
     // addProduct();
-  }, [])
 
-  // const DUMMY_DATA = [
-  //   {
-  //     key: 'm1',
-  //     name: 'Pineapple Cake',
-  //     description: 'Finest Cake',
-  //     price: 52.99,
-  //     img: pineappleCake
-  //   },
-  //   {
-  //     id: 'm2',
-  //     name: 'Carrot Cake',
-  //     description: 'A german specialty!',
-  //     price: 48.5,
-  //     img: carrotCake
-  //   },
-  //   {
-  //     id: 'm3',
-  //     name: 'Rasmalai Cake',
-  //     description: 'Indian, savvy!',
-  //     price: 62.99,
-  //     img: rasmalaiCake
-  //   },
-  //   {
-  //     id: 'm4',
-  //     name: 'Black Forest',
-  //     description: 'Chocolate King!',
-  //     price: 48.99,
-  //     img: blackForestCake
-  //   },
-  //   {
-  //     id: 'm5',
-  //     name: 'Mango Cake',
-  //     description: 'Seasonal Summer fever!',
-  //     price: 58.99,
-  //     img: mangoCheeseCake
-  //   },
-  //   {
-  //     id: 'm6',
-  //     name: 'Strawberry Cake',
-  //     description: 'Chocolate King!',
-  //     price: 48.99,
-  //     img: strawberryCake
-  //   },
-  //   {
-  //     id: 'm7',
-  //     name: 'BabyShower Theme',
-  //     description: 'Cream Cheese Frosting!',
-  //     price: 48.99,
-  //     img: babyShowerCake
-  //   },
-  //   {
-  //     id: 'm8',
-  //     name: 'Gulab Jamun Cake',
-  //     description: 'Cream Cheese Frosting!',
-  //     price: 48.99,
-  //     img: gulabJamunCake
-  //   },
-  //   {
-  //     id: 'm9',
-  //     name: 'Lemon Cake',
-  //     description: 'Cream Cheese Frosting!',
-  //     price: 48.99,
-  //     img: lemonCake
-  //   },
-  //   {
-  //     id: 'm10',
-  //     name: 'Chocolate Cake',
-  //     description: 'Cream Cheese Frosting!',
-  //     price: 48.99,
-  //     img: chocolateCake
-  //   },
-  //   {
-  //     id: 'm11',
-  //     name: 'Lemon Coconut Cake',
-  //     description: 'Awesome One!',
-  //     price: 68.99,
-  //     img: lemonCoconutCake
-  //   },
-  //   {
-  //     id: 'm12',
-  //     name: 'Muffins',
-  //     description: 'Added Bakery!',
-  //     price: 10.99,
-  //     img: chocolateWalnutCake
-  //   },
-  // ];
+  }, [])
 
   const listData = products.map((cake) => (
     <BakeItem
